7fb8704e0e41c8dd75f7d86e5941e22f4ffc4231
Pedro Henrique <125401919+30Lima@users.noreply.github.com>
2024-10-21 22:29:23 -0300

    WIP: Criação do arquivo 'update.sql'.

---
+++ DML.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_endereco (
+    cod_endereco,
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'TESTE',
+    4232,
+    '01308001',
+    'JARDIM',
+    'SAO PAULO CAPITAL',
+    'BRASIL',
+    'CIDADE ADEMAR'
+);
---
+++ update.sql
@@ -0,0 +1,8 @@
+UPDATE tb_endereco
+
+SET
+    bairro = 'PARAISO'
+WHERE
+    cod_endereco = 1;
+    
+COMMIT;
